31151829bca5b46fea321418c76e87f681a9c1dc
NivaDileep123 <65642377+NivaDileep123@users.noreply.github.com>
2020-08-09 21:43:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+(function(){
+	'use strict';
+	angular.module('LunchCheck',[])
+	.controller('LunchCheckController',LunchCheckController);
+	LunchCheckController.$inject=['$scope'];
+    function LunchCheckController($scope){
+    	$scope.items=''; 
+    	$scope.ans='';
+		$scope.displayMessage=function(){
+			    const after_split=$scope.items.split(',');
+			    var count=0;
+			    for(var i=0;i<after_split.length;i++){
+			    if(after_split[i]!=''){ //Not condidering empty items
+				    count=count+1;
+			     }}
+			     if(count==0){
+			     	$scope.ans="Please enter data first";
+			     }
+			    else if(count<=3){
+				     $scope.ans='Enjoy!';
+			       }
+			    else{
+				     $scope.ans='Too much!';
+			       }
+
+		};           
+    }
+		
+
+
+
+
+	})
+();
